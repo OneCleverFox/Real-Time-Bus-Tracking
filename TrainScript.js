6ed132eb09d0dbc1397a824f3c2d34ad13c3fc0a
OneCleverFox <124699699+OneCleverFox@users.noreply.github.com>
2023-04-16 20:09:43 +0200

    modified:   TrainScript.js 	modified:   index.html

--- TrainScript.js
+++ TrainScript.js
@@ -1,52 +1,57 @@
-const evaNumber = "8098360"; // EVA-Nummer der Station
-const url = `https://apis.deutschebahn.com/db-api-marketplace/apis/timetables/v1/fchg/${evaNumber}`;
-const headers = {
-  "Accept": "application/xml",
-  "DB-Client-Id": "6a0010ab1a1f6c80ac0e716cb7b78dd8",
-  "DB-Api-Key": "71c347e5d3ba42482aefd9fec6327b87"
-};
+const stationId = "8098360";
+  const apiKey = "71c347e5d3ba42482aefd9fec6327b87";
+  const clientId = "6a0010ab1a1f6c80ac0e716cb7b78dd8";
 
-// XMLHttpRequest-Objekt erstellen
-const xhr = new XMLHttpRequest();
+  getTrainDepartures(stationId, apiKey, clientId);
 
-xhr.open("GET", url, true);
 
-// Header setzen
-for (const key in headers) {
-  xhr.setRequestHeader(key, headers[key]);
-}
 
-// Callback-Funktion
-xhr.onload = function() {
-  if (xhr.status >= 200 && xhr.status < 300) {
-    // Antwort in XML umwandeln
-    const parser = new DOMParser();
-    const xmlDoc = parser.parseFromString(xhr.responseText, "text/xml");
-    
-    // Informationen aus der XML extrahieren
-    const trains = xmlDoc.getElementsByTagName("tl")[0].getElementsByTagName("t");
-    
-    // Train-Informationen in HTML-Liste einfügen
-    const trainList = document.getElementById("train-info");
-    for (let i = 0; i < trains.length; i++) {
-      const train = trains[i];
-      const time = train.getElementsByTagName("dp")[0].getAttribute("pt");
-      const destination = train.getElementsByTagName("ppth")[0].getAttribute("ppth");
-      const platform = train.getElementsByTagName("ppth")[0].getAttribute("pp");
-      const trainType = train.getAttribute("c");
-      const trainNumber = train.getAttribute("n");
-      const listItem = document.createElement("li");
-      listItem.innerHTML = `${time} - ${trainType} ${trainNumber} nach ${destination} (Gleis ${platform})`;
-      trainList.appendChild(listItem);
+// Funktion zum Abrufen von Zugabfahrten
+export function getTrainDepartures(stationId, apiKey, clientId) {
+  const evaNumber = stationId;
+  const url = `https://apis.deutschebahn.com/db-api-marketplace/apis/timetables/v1/fchg/${evaNumber}`;
+  const headers = {
+    "Accept": "application/xml",
+    "DB-Client-Id": clientId,
+    "DB-Api-Key": apiKey
+  };
+  
+  // XMLHttpRequest erstellen und konfigurieren
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", url);
+  xhr.setRequestHeader("Accept", headers.Accept);
+  xhr.setRequestHeader("DB-Client-Id", headers["DB-Client-Id"]);
+  xhr.setRequestHeader("DB-Api-Key", headers["DB-Api-Key"]);
+  
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      if (xhr.status === 200) {
+        // Erfolgreiche Antwort erhalten, Zugdaten parsen und anzeigen
+        const xmlData = xhr.responseXML;
+        const departures = xmlData.getElementsByTagName("s");
+        const trainList = document.getElementById("train-info");
+        
+        for (let i = 0; i < departures.length; i++) {
+          const train = departures[i];
+          const trainNumber = train.getAttribute("l");
+          const trainDestination = train.getAttribute("bs");
+          const trainDepartureTime = new Date(parseInt(train.getAttribute("dp")) * 1000);
+          
+          const listItem = document.createElement("li");
+          const listItemText = document.createTextNode(`Train ${trainNumber} to ${trainDestination}, departure at ${trainDepartureTime.toLocaleTimeString("de-DE")}`);
+          listItem.appendChild(listItemText);
+          trainList.appendChild(listItem);
+        }
+      } else {
+        // Fehler beim Abrufen der Daten, Fehlermeldung anzeigen
+        const trainList = document.getElementById("train-info");
+        const listItem = document.createElement("li");
+        const listItemText = document.createTextNode("Error loading train departures");
+        listItem.appendChild(listItemText);
+        trainList.appendChild(listItem);
+      }
     }
-  } else {
-    console.error(xhr.statusText);
-  }
-};
-
-xhr.onerror = function() {
-  console.error(xhr.statusText);
-};
-
-// Anfrage senden
-xhr.send();
+  };
+  
+  xhr.send();
+}
